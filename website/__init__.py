d4af19d8a634c3606ce3687243b7ba5a773cb6ed
unknown <gyeowj@gmail.com>
2024-07-05 10:46:45 +0800

    Change Dockerfile to use gunicorn

--- website/__init__.py
+++ website/__init__.py
@@ -4,13 +4,12 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres-db:plschangeme@svc-postgres-headless:5432/postgres-db'
     db.init_app(app)
 
     from .views import views
